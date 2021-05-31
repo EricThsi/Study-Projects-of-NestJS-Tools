1146210c49631c199683365cfe39008e9adc7633
EricThsi <beefjava@gmail.com>
2021-05-31 21:50:50 +0800

    feat(app): add csv parser

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CsvModule } from 'nest-csv-parser';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), CsvModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { CsvParser, ParsedData } from 'nest-csv-parser';
+import * as fs from 'fs';
+import * as path from 'path';
+
+class Entity {
+  username: string;
+  password: string;
+  role: string;
+}
 
 @Injectable()
 export class AppService {
+  constructor(private readonly csvParser: CsvParser) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async parse() {
+    const dataSrc = path.join(__dirname, '..', 'data/demo.csv');
+    const stream = fs.createReadStream(dataSrc);
+    const entities: ParsedData<InstanceType<any>> = await this.csvParser.parse(
+      stream,
+      Entity,
+      null,
+      null,
+      {
+        separator: ',',
+      },
+    );
+    return entities;
   }
 }
